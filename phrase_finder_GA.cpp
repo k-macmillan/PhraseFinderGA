09c8456190c55499bb7c5baf134c198666260655
Kyle <macattackftw@gmail.com>
2017-05-28 02:08:46 -0600

    Added user input to select pool size and mutation rate. Implemented random string build based on user specifications.

--- phrase_finder_GA.cpp
+++ phrase_finder_GA.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>
+
+#define GET_RAND rand() % 95 + 32	// 32 - 126
 
 using namespace std;
 
@@ -10,6 +15,31 @@ int main(int argc, char** argv)
 		cout << "Too few arguments! Enter a phrase starting argument:\nphrase_finder_GA To boldly go.\n";
 		return 1;
 	}
+
+	srand(time(NULL));
+
+	string words = "";
+	for (int i = 1; i < argc; ++i)
+		words += string(argv[i]) + " ";
 	
+	words.pop_back();
+
+	unsigned int pool_size, mutation;
+	cout << "Enter desired pool size: ";
+	cin >> pool_size;
+
+	cout << "\nEnter desired mutation rate in percent(ex: 5): ";
+	cin >> mutation;
+
+	vector<string> pool(pool_size);
+	for (unsigned int i = 0; i < pool_size; ++i)
+	{
+		string random_str;
+		for (unsigned int j = 0; j < words.size(); ++j)
+			random_str += GET_RAND;
+		pool[i] = random_str;
+	}
+
+
 	return 0;
 }
