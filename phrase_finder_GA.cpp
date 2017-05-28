b3541d0927baa1c68f55b6f1392b4c807b4f4fb0
Kyle <macattackftw@gmail.com>
2017-05-28 02:33:39 -0600

    Made pool mutation function. Each character has an equal chance to mutate based on user desired mutation rate.

--- phrase_finder_GA.cpp
+++ phrase_finder_GA.cpp
@@ -8,6 +8,8 @@
 
 using namespace std;
 
+void Mutate(vector<string> &v, int mutation);
+
 int main(int argc, char** argv)
 {
 	if (argc < 2)
@@ -24,22 +26,36 @@ int main(int argc, char** argv)
 	
 	words.pop_back();
 
-	unsigned int pool_size, mutation;
+	int pool_size, mutation;
 	cout << "Enter desired pool size: ";
 	cin >> pool_size;
 
-	cout << "\nEnter desired mutation rate in percent(ex: 5): ";
+	cout << "\nEnter desired mutation rate in integer percentages(ex: 5): ";
 	cin >> mutation;
 
 	vector<string> pool(pool_size);
-	for (unsigned int i = 0; i < pool_size; ++i)
+	for (int i = 0; i < pool_size; ++i)
 	{
 		string random_str;
 		for (unsigned int j = 0; j < words.size(); ++j)
 			random_str += GET_RAND;
-		pool[i] = random_str;
+		pool[i] = random_str;	
 	}
+
+	std::vector<string> v = pool;
+	Mutate(pool, mutation);
+
+	for (unsigned int i = 0; i < v.size(); ++i)
+		cout << v[i] << endl << pool[i] << endl << endl;
 
 
 	return 0;
+}
+
+void Mutate(vector<string> &v, int mutation)
+{
+	for (unsigned int i = 0; i < v.size(); ++i)
+		for (unsigned int j = 0; j < v[i].size(); ++j)
+			if (rand() % 101 <= mutation)
+				v[i][j] = GET_RAND;	// small chance the mutation is the same value, deciding to ignore		
 }
