889579d8a4782603515aeebbd921bfdd8da17577
Kyle <macattackftw@gmail.com>
2017-05-28 01:36:02 -0600

    Project started. Created main and makefile, builds.

---
+++ phrase_finder_GA.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	if (argc < 2)
+	{
+		cout << "Too few arguments! Enter a phrase starting argument:\nphrase_finder_GA To boldly go.\n";
+		return 1;
+	}
+	
+	return 0;
+}
